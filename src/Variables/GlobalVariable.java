2abc29446c8c5fae9caf9fe6726fa2a989ca82d4
LTianRahul <kkrh00@gmail.com>
2023-05-30 13:33:11 +0530

    added Java Tutorial File

---
+++ src/Variables/GlobalVariable.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class GlobalVariable {
+	
+	static int a;  // static int a=0;
+	static int b=5;  // 
+
+	public static void main(String[] args) {
+		System.out.println(b);
+		
+		//codespark567@gmail.com
+
+	}
+
+}
---
+++ src/Variables/LocalVariable.java
@@ -0,0 +1,18 @@
+package Variables;
+
+public class LocalVariable {
+	
+	static void test()
+	{
+		int a=5;
+		System.out.println(a);
+	}
+	public static void main(String[] args) {
+		
+		int a;
+		a=10;//D & I
+		System.out.println(a);
+		
+	}
+
+}
